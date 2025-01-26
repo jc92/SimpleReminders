b6c4d64c3e2ee70e1b0074ac11e6898a59d44dcf
JC Ton <tonjcl@gmail.com>
2025-01-26 13:26:39 +0100

    Fix actor isolation in main.swift

--- Sources/SimpleReminders/main.swift
+++ Sources/SimpleReminders/main.swift
@@ -2,7 +2,11 @@ import AppKit
 
 // Create the application and delegate
 let app = NSApplication.shared
-let delegate = AppDelegate()
-app.delegate = delegate
-app.setActivationPolicy(.accessory)  // This makes it a menu bar app
-app.run()
+
+// Initialize delegate on the main actor
+Task { @MainActor in
+    let delegate = AppDelegate()
+    app.delegate = delegate
+    app.setActivationPolicy(.accessory)  // This makes it a menu bar app
+    app.run()
+}
